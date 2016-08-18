5f2c20bacbc7152e24a95697cb5bfcc1a3106733
alin.rautoiu <alin.rautoiu@teamnet.ro>
2016-08-18 15:21:26 +0300

    Response headers

--- webcomicr.js
+++ webcomicr.js
@@ -89,7 +89,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin');
+    res.setHeader('Access-Control-Allow-Origin', '');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
